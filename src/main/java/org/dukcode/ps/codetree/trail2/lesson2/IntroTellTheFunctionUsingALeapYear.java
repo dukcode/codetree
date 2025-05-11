6552ed7d46761deafb3861ecd318919fe762a14f
dukecode <duk9741@gmail.com>
2025-05-11 16:01:18 +0900

    trail2: IntroTellTheFunctionUsingALeapYear

---
+++ src/main/java/org/dukcode/ps/codetree/trail2/lesson2/IntroTellTheFunctionUsingALeapYear.java
@@ -0,0 +1,35 @@
+package org.dukcode.ps.codetree.trail2.lesson2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a
+ * href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-tell-the-function-using-a-leap-year/description">LINK</a>
+ */
+public class IntroTellTheFunctionUsingALeapYear {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int year = Integer.parseInt(br.readLine());
+    bw.write(isLeapYear(year) ? "true" : "false");
+
+    br.close();
+    bw.close();
+
+  }
+
+  private static boolean isLeapYear(int year) {
+    return (year % 4 == 0) && !(year % 100 == 0 && year % 400 != 0);
+  }
+}
