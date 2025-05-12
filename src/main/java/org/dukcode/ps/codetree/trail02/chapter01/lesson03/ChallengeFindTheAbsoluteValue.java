229fee301474961d0d0849aee62079672ee784b4
김덕윤 <duk9741@gmail.com>
2025-05-12 13:28:05 +0900

    trail2: ChallengeFindTheAbsoluteValue

---
+++ src/main/java/org/dukcode/ps/codetree/trail02/chapter01/lesson03/ChallengeFindTheAbsoluteValue.java
@@ -0,0 +1,51 @@
+package org.dukcode.ps.codetree.trail02.chapter01.lesson03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a
+ * href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-find-the-absolute-value/description">LINK</a>
+ */
+public class ChallengeFindTheAbsoluteValue {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+
+    st = new StringTokenizer(br.readLine());
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    toAbsoluteValue(arr);
+
+    for (int num : arr) {
+      bw.write(String.valueOf(num));
+      bw.write(' ');
+    }
+
+    br.close();
+    bw.close();
+
+  }
+
+  private static void toAbsoluteValue(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] < 0) {
+        arr[i] *= -1;
+      }
+    }
+  }
+}
