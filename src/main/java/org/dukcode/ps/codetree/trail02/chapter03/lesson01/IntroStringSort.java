9e095a524aa9e78aa6c7e15fbfad7e381dc6d0f7
dukecode <duk9741@gmail.com>
2025-05-14 01:13:27 +0900

    trail2: IntroStringSort

---
+++ src/main/java/org/dukcode/ps/codetree/trail02/chapter03/lesson01/IntroStringSort.java
@@ -0,0 +1,33 @@
+package org.dukcode.ps.codetree.trail02.chapter03.lesson01;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a
+ * href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-string-sort/description">LINK</a>
+ */
+public class IntroStringSort {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    char[] word = br.readLine().toCharArray();
+    Arrays.sort(word);
+    bw.write(String.valueOf(word));
+
+    br.close();
+    bw.close();
+
+  }
+}
