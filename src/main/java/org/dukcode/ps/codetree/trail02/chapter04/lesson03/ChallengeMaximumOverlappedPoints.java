a23ac186d832dcd638262f50052e758e3c0caf81
dukecode <duk9741@gmail.com>
2025-05-17 18:32:53 +0900

    trail2: ChallengeMaximumOverlappedPoints

---
+++ src/main/java/org/dukcode/ps/codetree/trail02/chapter04/lesson03/ChallengeMaximumOverlappedPoints.java
@@ -0,0 +1,47 @@
+package org.dukcode.ps.codetree.trail02.chapter04.lesson03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a
+ * href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-maximum-overlapped-points/description">LINK</a>
+ */
+public class ChallengeMaximumOverlappedPoints {
+
+  private static final int RANGE = 100;
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[] lines = new int[2 * RANGE + 1];
+
+    int ans = 0;
+    int n = Integer.parseInt(br.readLine());
+    for (int i = 0; i < n; i++) {
+      st = new StringTokenizer(br.readLine());
+      int frIn = Integer.parseInt(st.nextToken());
+      int toIn = Integer.parseInt(st.nextToken());
+
+      for (int idx = frIn; idx <= toIn; idx++) {
+        lines[idx + RANGE]++;
+        ans = Math.max(ans, lines[idx + RANGE]);
+      }
+    }
+
+    bw.write(String.valueOf(ans));
+
+    br.close();
+    bw.close();
+
+  }
+}
