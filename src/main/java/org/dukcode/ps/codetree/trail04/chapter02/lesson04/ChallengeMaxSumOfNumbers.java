e176b5962e257f9aea198615b63249bab53f9381
김덕윤 <duk9741@gmail.com>
2025-08-12 18:08:21 +0900

    trail4: ChallengeMaxSumOfNumbers

---
+++ src/main/java/org/dukcode/ps/codetree/trail04/chapter02/lesson04/ChallengeMaxSumOfNumbers.java
@@ -0,0 +1,66 @@
+package org.dukcode.ps.codetree.trail04.chapter02.lesson04;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a
+ * href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-max-sum-of-numbers/description">LINK</a>
+ */
+public class ChallengeMaxSumOfNumbers {
+
+  private static final int MN = -987_654_321;
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+  private static StringTokenizer st;
+
+  private static int n;
+  private static int[][] board;
+
+  private static boolean[] taken;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    n = Integer.parseInt(br.readLine());
+    board = new int[n][n];
+    for (int y = 0; y < n; y++) {
+      st = new StringTokenizer(br.readLine());
+      for (int x = 0; x < n; x++) {
+        board[y][x] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    taken = new boolean[n];
+    bw.write(String.valueOf(solve(0)));
+
+    br.close();
+    bw.close();
+
+  }
+
+  private static int solve(int y) {
+    if (y == n) {
+      return 0;
+    }
+
+    int ret = MN;
+    for (int x = 0; x < n; ++x) {
+      if (taken[x]) {
+        continue;
+      }
+
+      taken[x] = true;
+      ret = Math.max(ret, solve(y + 1) + board[y][x]);
+      taken[x] = false;
+    }
+
+    return ret;
+  }
+}
