86bb4156e21a9e777fda70460fb979d8626749e4
김덕윤 <duk9741@gmail.com>
2025-09-22 13:35:08 +0900

    trail5: ChallengeArrayExtraction

---
+++ src/main/java/org/dukcode/ps/codetree/trail05/chapter01/lesson05/ChallengeArrayExtraction.java
@@ -0,0 +1,44 @@
+package org.dukcode.ps.codetree.trail05.chapter01.lesson05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @see <a
+ * href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-array-extraction/description">LINK</a>
+ */
+public class ChallengeArrayExtraction {
+
+  private static BufferedReader br;
+  private static BufferedWriter bw;
+
+  private static int n;
+  private static PriorityQueue<Integer> pq;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    n = Integer.parseInt(br.readLine());
+
+    pq = new PriorityQueue<>(Comparator.reverseOrder());
+    for (int i = 0; i < n; i++) {
+      int num = Integer.parseInt(br.readLine());
+
+      if (num == 0) {
+        bw.write(String.valueOf(pq.isEmpty() ? 0 : pq.poll()));
+        bw.newLine();
+      } else {
+        pq.offer(num);
+      }
+    }
+
+    br.close();
+    bw.close();
+  }
+}
