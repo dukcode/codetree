16461942f7a6de830adb58904d1fa2828f7ac9d4
dukecode <duk9741@gmail.com>
2026-04-06 00:26:02 +0900

    trail1: IntroPrintQuote

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail01/chapter01/lesson01/IntroPrintQuote.kt
@@ -0,0 +1,20 @@
+package org.dukcode.ps.codetree.trail01.chapter01.lesson01
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.*
+
+/**
+ * @see [LINK](https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-quote/description)
+ */
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+private var st: StringTokenizer? = null
+
+fun main() {
+    bw.write("""He says "It's a really simple sentence".""")
+
+    bw.flush()
+}
