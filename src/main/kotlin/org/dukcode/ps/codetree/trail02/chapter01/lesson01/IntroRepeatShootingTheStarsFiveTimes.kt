0ae3d4ba41eb2ebc67c9d45f6d985b310598bacc
dukecode <duk9741@gmail.com>
2026-04-06 10:14:27 +0900

    trail2: IntroRepeatShootingTheStarsFiveTimes

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson01/IntroRepeatShootingTheStarsFiveTimes.kt
@@ -0,0 +1,30 @@
+package org.dukcode.ps.codetree.trail02.chapter01.lesson01
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+/**
+ * @see [LINK](https://www.codetree.ai/ko/trails/complete/curated-cards/intro-repeat-shooting-the-stars-five-times/description)
+ */
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+private var st: StringTokenizer? = null
+
+fun main() {
+    repeat(5) {
+        bw.write(createRow(10))
+        bw.newLine()
+    }
+
+    bw.flush()
+}
+
+fun createRow(col: Int): String =
+    buildString {
+        repeat(col) {
+            append("*")
+        }
+    }
