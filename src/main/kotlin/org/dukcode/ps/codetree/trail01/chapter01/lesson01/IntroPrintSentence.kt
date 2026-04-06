0d2d1f7345b3b176c6eca74a435a364686006264
dukecode <duk9741@gmail.com>
2026-04-06 13:17:34 +0900

    refactor: Kotlin doc 주석 위치를 fun main() 위로 이동

--- src/main/kotlin/org/dukcode/ps/codetree/trail01/chapter01/lesson01/IntroPrintSentence.kt
+++ src/main/kotlin/org/dukcode/ps/codetree/trail01/chapter01/lesson01/IntroPrintSentence.kt
@@ -6,13 +6,13 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.StringTokenizer
 
-/**
- * @see [LINK](https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-sentence/description)
- */
 private val br = BufferedReader(InputStreamReader(System.`in`))
 private val bw = BufferedWriter(OutputStreamWriter(System.out))
 private var st: StringTokenizer? = null
 
+/**
+ * @see [LINK](https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-sentence/description)
+ */
 fun main() {
     bw.write("Hello World!")
 
--- src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson01/IntroRepeatShootingTheStarsFiveTimes.kt
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson01/IntroRepeatShootingTheStarsFiveTimes.kt
@@ -6,13 +6,13 @@ import java.io.InputStreamReader
 import java.io.OutputStreamWriter
 import java.util.StringTokenizer
 
-/**
- * @see [LINK](https://www.codetree.ai/ko/trails/complete/curated-cards/intro-repeat-shooting-the-stars-five-times/description)
- */
 private val br = BufferedReader(InputStreamReader(System.`in`))
 private val bw = BufferedWriter(OutputStreamWriter(System.out))
 private var st: StringTokenizer? = null
 
+/**
+ * @see [LINK](https://www.codetree.ai/ko/trails/complete/curated-cards/intro-repeat-shooting-the-stars-five-times/description)
+ */
 fun main() {
     repeat(5) {
         bw.write(createRow(10))
