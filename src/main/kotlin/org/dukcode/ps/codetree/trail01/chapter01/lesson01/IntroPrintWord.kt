f1e936beeeea33acda21809974a257b50d5e1d83
dukecode <duk9741@gmail.com>
2026-04-06 10:18:41 +0900

    chore: spotless apply

--- src/main/kotlin/org/dukcode/ps/codetree/trail01/chapter01/lesson01/IntroPrintWord.kt
+++ src/main/kotlin/org/dukcode/ps/codetree/trail01/chapter01/lesson01/IntroPrintWord.kt
@@ -4,7 +4,7 @@ import java.io.BufferedReader
 import java.io.BufferedWriter
 import java.io.InputStreamReader
 import java.io.OutputStreamWriter
-import java.util.*
+import java.util.StringTokenizer
 
 /**
  * @see [LINK](https://www.codetree.ai/ko/trails/complete/curated-cards/intro-print-word/description)
