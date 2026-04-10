544b5b7e2cacea0ebe079d023119c1c36dc9929e
dukecode <duk9741@gmail.com>
2026-04-10 22:22:25 +0900

    trail2: IntroAnEvenNumberWithAMultipleOf5InTheSum

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson02/IntroAnEvenNumberWithAMultipleOf5InTheSum.kt
@@ -0,0 +1,39 @@
+package org.dukcode.ps.codetree.trail02.chapter01.lesson02
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-an-even-number-with-a-multiple-of-5-in-the-sum/description">LINK</a>
+ */
+fun main() {
+    val num = br.readLine().toInt()
+    bw.write(if (isMagicNumber(num)) "Yes" else "No")
+
+    bw.flush()
+}
+
+fun isMagicNumber(num: Int): Boolean = num % 2 == 0 && digitSum(num) % 5 == 0
+
+fun digitSum(num: Int): Int {
+    var ret = 0
+
+    var value = num
+    while (value > 0) {
+        ret += value % 10
+        value /= 10
+    }
+
+    return ret
+}
+
+/*
+fun digitSum2(num: Int): Int = generateSequence(num) { it / 10 }
+        .takeWhile { it > 0 }
+        .sumOf { it % 10 }
+*/
