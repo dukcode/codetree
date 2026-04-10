7101bfea433480bd6f992c4684fc028415af067f
dukecode <duk9741@gmail.com>
2026-04-10 22:28:02 +0900

    trail2: Intro369GamesUsingFunctions

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson02/Intro369GamesUsingFunctions.kt
@@ -0,0 +1,53 @@
+package org.dukcode.ps.codetree.trail02.chapter01.lesson02
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-369-games-using-functions/description">LINK</a>
+ */
+fun main() {
+    val st = StringTokenizer(br.readLine())
+
+    val a = st.nextToken().toInt()
+    val b = st.nextToken().toInt()
+
+    bw.write(count369(a, b).toString())
+
+    bw.flush()
+}
+
+fun count369(
+    frIn: Int,
+    toIn: Int,
+): Int {
+    var ret = 0
+    for (num in frIn..toIn) {
+        if (has369(num) || num % 3 == 0) {
+            ret++
+        }
+    }
+
+    return ret
+}
+
+fun has369(num: Int): Boolean {
+    var value = num
+    while (value > 0) {
+        val digit = value % 10
+
+        if (digit == 3 || digit == 6 || digit == 9) {
+            return true
+        }
+
+        value /= 10
+    }
+
+    return false
+}
