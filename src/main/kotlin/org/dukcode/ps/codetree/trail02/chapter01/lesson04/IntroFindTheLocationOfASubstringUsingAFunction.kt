77c087a0eb284d4d2f99f1d9a72ac940b6cb0ea3
김덕윤 <duk9741@gmail.com>
2026-04-18 20:58:46 +0900

    trail2: IntroFindTheLocationOfASubstringUsingAFunction

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson04/IntroFindTheLocationOfASubstringUsingAFunction.kt
@@ -0,0 +1,6 @@
+package org.dukcode.ps.codetree.trail02.chapter01.lesson04.introFindTheLocationOfASubstringUsingAFunction
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-find-the-location-of-a-substring-using-a-function/description">LINK</a>
+ */
+fun main() = print(readln().indexOf(readln()))
