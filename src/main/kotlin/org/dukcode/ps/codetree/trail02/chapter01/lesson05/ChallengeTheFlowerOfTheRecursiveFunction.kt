21ff214bb91a129e70fff748a015a1d0bb5e3d3f
김덕윤 <duk9741@gmail.com>
2026-04-18 23:26:48 +0900

    trail2: ChallengeTheFlowerOfTheRecursiveFunction

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson05/ChallengeTheFlowerOfTheRecursiveFunction.kt
@@ -0,0 +1,32 @@
+package org.dukcode.ps.codetree.trail02.chapter01.lesson05.challengeTheFlowerOfTheRecursiveFunction
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-the-flower-of-the-recursive-function/description">LINK</a>
+ */
+fun main() {
+    val n = br.readLine().toInt()
+
+    printNums(n)
+
+    bw.flush()
+}
+
+private fun printNums(n: Int) {
+    if (n == 0) {
+        return
+    }
+
+    bw.write("$n ")
+
+    printNums(n - 1)
+
+    bw.write("$n ")
+}
