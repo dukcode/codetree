759eacc43b3ada3a8e37c31798de09f6fc20269e
김덕윤 <duk9741@gmail.com>
2026-04-18 23:16:33 +0900

    trail2: IntroRepeatedOutput2

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter01/lesson05/IntroRepeatedOutput2.kt
@@ -0,0 +1,30 @@
+package org.dukcode.ps.codetree.trail02.chapter01.lesson05.introRepeatedOutput2
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-repeated-output-2/description">LINK</a>
+ */
+fun main() {
+    val n = br.readLine().toInt()
+    printHelloWorld(n)
+
+    bw.flush()
+}
+
+fun printHelloWorld(n: Int) {
+    if (n == 0) {
+        return
+    }
+
+    bw.write("HelloWorld")
+    bw.newLine()
+
+    printHelloWorld(n - 1)
+}
