1a1b742971403f7f27c28deddcbce50daa2fb52d
김덕윤 <duk9741@gmail.com>
2026-04-18 23:48:31 +0900

    trail2: IntroSquareOfEachDigit

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter02/lesson02/IntroSquareOfEachDigit.kt
@@ -0,0 +1,30 @@
+package org.dukcode.ps.codetree.trail02.chapter02.lesson02.introSquareOfEachDigit
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/intro-square-of-each-digit/description">LINK</a>
+ */
+fun main() {
+    val n = br.readLine().toInt()
+
+    bw.write(squareDigitSum(n).toString())
+
+    bw.flush()
+}
+
+fun squareDigitSum(n: Int): Int {
+    if (n == 0) {
+        return 0
+    }
+
+    val digit = n % 10
+
+    return digit * digit + squareDigitSum(n / 10)
+}
