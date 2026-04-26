13d6b8636fe37112d68551bda4e7d8576f690286
김덕윤 <duk9741@gmail.com>
2026-04-26 22:52:26 +0900

    trail2: ChallengeContinuousNumber4

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter05/lesson01/ChallengeContinuousNumber4.kt
@@ -0,0 +1,46 @@
+package org.dukcode.ps.codetree.trail02.chapter05.lesson01.challengeContinuousNumber4
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-continuous-number4/description">LINK</a>
+ */
+fun main() {
+    val n = br.readLine().toInt()
+    val arr =
+        IntArray(n) {
+            br.readLine().toInt()
+        }
+
+    var maxCnt = 0
+    var cnt = 0
+    for (idx in arr.indices) {
+        if (idx == 0 || arr[idx - 1] >= arr[idx]) {
+            maxCnt = maxOf(maxCnt, cnt)
+            cnt = 1
+            continue
+        }
+
+        cnt++
+    }
+
+    maxCnt = maxOf(maxCnt, cnt)
+
+    bw.write(maxCnt.toString())
+
+    bw.flush()
+}
+
+private fun parseInts() =
+    StringTokenizer(br.readLine()).run {
+        IntArray(countTokens()) {
+            nextToken().toInt()
+        }
+    }
