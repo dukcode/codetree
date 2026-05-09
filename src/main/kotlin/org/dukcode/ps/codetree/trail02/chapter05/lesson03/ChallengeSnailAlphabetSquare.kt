6bf8e0b8cf8e42329fb863a3fb5f20cb425d8b8f
dukecode <duk9741@gmail.com>
2026-05-09 21:13:17 +0900

    trail2: ChallengeSnailAlphabetSquare

---
+++ src/main/kotlin/org/dukcode/ps/codetree/trail02/chapter05/lesson03/ChallengeSnailAlphabetSquare.kt
@@ -0,0 +1,51 @@
+package org.dukcode.ps.codetree.trail02.chapter05.lesson03.challengeSnailAlphabetSquare
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.util.StringTokenizer
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private val bw = BufferedWriter(OutputStreamWriter(System.out))
+
+// RIGHT, DOWN, LEFT, UP
+private val dy = intArrayOf(0, 1, 0, -1)
+private val dx = intArrayOf(1, 0, -1, 0)
+
+/**
+ * @see <a href="https://www.codetree.ai/ko/trails/complete/curated-cards/challenge-snail-number-square-2/description">LINK</a>
+ */
+fun main() {
+    val (h, w) = parseInts()
+
+    val board = Array(h) { CharArray(w) }
+
+    var y = 0
+    var x = 0
+    var dir = 0
+    repeat(h * w) {
+        board[y][x] = 'A' + (it % 26)
+
+        val ny = y + dy[dir]
+        val nx = x + dx[dir]
+
+        if (ny !in 0 until h || nx !in 0 until w || board[ny][nx] != '\u0000') {
+            dir = (dir + 1) % 4
+        }
+
+        y += dy[dir]
+        x += dx[dir]
+    }
+
+    bw.write(board.joinToString("\n") { it.joinToString(" ") })
+
+    bw.flush()
+}
+
+private fun parseInts() =
+    StringTokenizer(br.readLine()).run {
+        IntArray(countTokens()) {
+            nextToken().toInt()
+        }
+    }
